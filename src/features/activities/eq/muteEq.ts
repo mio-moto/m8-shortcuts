455c8ae64f894e57bd5483ef85d1e368b13629d2
Mio Moto <6331542+mio-moto@users.noreply.github.com>
2025-11-08 18:24:32 +0100

    feat: new eq videos

--- src/features/activities/eq/muteEq.ts
+++ src/features/activities/eq/muteEq.ts
@@ -1,11 +1,12 @@
-import Video from '#assets/activity/eq/mute-eq.webm'
+import Data from '#assets/activity/eq/mute-eq.json'
+import Video from '#assets/activity/eq/mute-eq.mp4'
 import type { Activity } from '../../activity'
 
 export const muteEq = {
   id: 'mute-eq',
   title: 'Mute EQ',
   description: 'Enables / disables the current EQ',
-  media: { video: Video },
+  media: { video: Video, events: Data as [number, number][] },
   keypress: ['edit', 'after', 'edit'],
   categories: ['misc'],
 } as const satisfies Activity
